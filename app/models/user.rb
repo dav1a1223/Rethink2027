6a4f06f9f2756935f1d17a439fa87bfe0ca993d4
dalai <dav1a1223@gmail.com>
2017-09-15 15:57:59 +0800

    email format validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   has_one :proposal
+  validates :email, :presence => true, :email => true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
